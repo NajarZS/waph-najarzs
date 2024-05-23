f2d7f22d2f0371f6fe81dcd022e7b041a9407a2b
NajarZS <najarzs@mail.uc.edu>
2024-05-22 23:49:09 -0400

    Create index.c

---
+++ Labs/Lab1/index.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+
+int main(void) {
+
+    printf("Content-Type: text/html; charset=utf-8\n\n");
+
+    printf("<!DOCTYPE html>\n");
+
+    printf("<html>\n");
+
+    printf("<head>\n");
+
+    printf("<title>WAPH- Zaid Najar</title>\n"); 
+
+    printf("</head>\n");
+
+    printf("<body>\n");
+
+    printf("<h1>Welcome to WAPH! I am Zaid Najar.</h1>\n"); 
+
+    printf("<p> I am a Computer Science Major, class of 2025.</p>\n"); 
+
+    printf("</body>\n");
+
+    printf("</html>\n");
+
+    return 0;
+
+}
